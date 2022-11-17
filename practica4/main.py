52c21046ed1f78f09a764e4435e9ce8285870b33
SabrinaBenitez <slbprofesional@gmail.com>
2022-11-17 12:16:19 -0300

    Correcciones sobre practico 4

--- practica4/main.py
+++ practica4/main.py
@@ -1,13 +1,9 @@
-import logging
-import logging.config
-from functions  import count_line,count_words
+import functions as f
+from os import path
 
-#logging.config.fileConfig(fname='log_config_file', disable_existing_loggers=False)
-#logger = logging.getLogger('mainLogger')
-
-def main ():
-    count_line('cuento.txt')
-    count_words()
+file= path.join(path.dirname(path.abspath('cuento.txt')), 'cuento.txt')
+def main():
+    f.count_line(file)
 
 if __name__ == '__main__':
     main()
