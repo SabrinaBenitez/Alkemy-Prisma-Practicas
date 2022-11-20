05dc8a730bc63d96f6ff78e83151642317c3c48d
= <=>
2022-11-20 18:58:45 -0300

    Correcciones practica 9

--- practica9/main.py
+++ practica9/main.py
@@ -1,7 +1,11 @@
-from obtener_transporte import obtener_medio_transporte
-
+from transporte import obtener_medio_transporte
 
 if __name__ == '__main__':
     medios_transporte = ['auto', 'avión', 'barco', 'bicicleta', 'ómnibus']
+ 
     while True:
-        print(obtener_medio_transporte(medios_transporte))
+        #indice = int(input("Ingrese Indice:"))
+        transporte = obtener_medio_transporte(medios_transporte)
+        print('Medios de Transporte:'+ transporte)
+       
+           
